31a7f28ba37b8f343c1fcacfce45987fbc676f33
Naman Madan <namanmadan2000@gmail.com>
2020-03-20 17:14:28 -0400

    Added documentation

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 public class Gameplay {
-	int recruited;
-	int econ_stability;
-	int caught_probability;
-	int total_pop;
-	Player player;
-	HashMap<String, Location> globe = new HashMap<String,Location>();
-	String startLocation;
-	String[] options = new String[] {"Recruit Within", "Spread Outside", 
+	private int recruited;
+	private int econ_stability;
+	private int caught_probability;
+	private int total_pop;
+	private Player player;
+	private HashMap<String, Location> globe = new HashMap<String,Location>();
+	private String startLocation;
+	private String[] options = new String[] {"Recruit Within", "Spread Outside", 
 	"Deceive Feds"};
-	int deception_counter = 0;
+	private int deception_counter = 0;
 	
 	/**
-	 * Allows the game to play out
+	 * Allows the game to play out. Initializes all the member variables to a 
+	 * reasonable setting
 	 * @param players the players available in the game
 	 */
 	public Gameplay(Player[] players, HashMap<String, Location> locations){
@@ -29,7 +30,12 @@ public class Gameplay {
 		buildScheme();
 	}
 	
-	
+	/**
+	 * Prints out the current percentages of those recruited, economic stability
+	 * factor, and probability of getting caught. Checks to make sure that player
+	 * has not been caught nor has won. Then continues game by allowing player
+	 * to choose options
+	 */
 	private void buildScheme() {
 		recruited = 0;
 		for (Location l : globe.values()) {
@@ -43,13 +49,32 @@ public class Gameplay {
 		
 		System.out.println(econ_stability + ": Economic Stability out of 100");
 		System.out.println(caught_probability + "% : Probability of being caught \n");
-		listOptions();
-		if (checkCaught()) {
+		
+		if (checkCaught() || checkWin()) {
 			return;
+		}else {
+			listOptions();
+			buildScheme();	
 		}
-		buildScheme();	
+		
 	}
 	
+	/**
+	 * Returns whether or not the player has won
+	 * @return true if the number recruited is equal to the total population
+	 */
+	private boolean checkWin() {
+		if (recruited == total_pop) {
+			System.out.println("Congratulations! Your Ponzi Scheme has captured everyone");
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Lists the options that the player has, asks user to choose and option,
+	 * and verifies that the option is valid
+	 */
 	private void listOptions() {
 		System.out.println("Choose the number of the option you would like");
 		int counter= 0;
@@ -76,6 +101,11 @@ public class Gameplay {
 		makeDecision(choice);
 	}
 	
+	/**
+	 * Selects the proper decision to be made
+	 * @param choice is the number value of the choice based on the choices 
+	 * member variable
+	 */
 	private void makeDecision(int choice) {
 		switch(choice) {
 		case 1: 
@@ -92,11 +122,18 @@ public class Gameplay {
 		}
 	}
 	
+	/**
+	 * Calculates the new number of recruits within the starting location
+	 */
 	private void recruitWithin() {
 		Location current = globe.get(startLocation);
 		calculateNewRecruits(current);
 	}
 	
+	/**
+	 * calculates the new number of recruits in current
+	 * @param current the location to change the number of recruits
+	 */
 	private void calculateNewRecruits(Location current) {
 		int curr_pop = current.getPopulation();
 		int curr_recruited = current.getRecruited();
@@ -106,14 +143,17 @@ public class Gameplay {
 		int risk = player.getRisk();
 		int strategy = player.getStrategy();
 		int likeability = player.getLikeability();
+		int experience = player.getExperience();
 		
 		int newRecruits = (int) (Math.random() * curr_pop / 50);
 		newRecruits += (int) (Math.random() * 5) * curr_density;
 		//Gives a 1/3 chance to increase the amount of recruited by the total GDP
 		newRecruits += (int) (Math.random() * 3) == 0  ? curr_gdp : 0;
 		
-		newRecruits += (int) (Math.random() * 12) < strategy ? strategy * 10000 : 0;
-		newRecruits += (int) (Math.random() * 12) < likeability ? likeability * 10000 : 0;
+		newRecruits += (int) (Math.random() * 12) < strategy ? strategy * 20000 : 0;
+		newRecruits += (int) (Math.random() * 12) < likeability ? likeability * 20000 : 0;
+		newRecruits += (int) (Math.random() * 12) < risk ? risk * 10000 : 0;
+		newRecruits += (int) (Math.random() * 12) < experience ? experience * 10000 : 0;
 		
 		
 		int newTotal = newRecruits + curr_recruited;
@@ -130,6 +170,10 @@ public class Gameplay {
 		globe.replace(current.getName(), current);
 	}
 	
+	/**
+	 * Asks user for location to spread and verifies that it is a valid location
+	 * Calculates new recruits in this location
+	 */
 	private void spreadOutside() {
 		System.out.println("Select the name of the location where you would like to spread your Ponzi Scheme");
 		for (Location l : globe.values()) {
@@ -154,6 +198,9 @@ public class Gameplay {
 		calculateNewRecruits(globe.get(choice));
 	}
 	
+	/**
+	 * reduces the probability of getting caught
+	 */
 	private void deceiveFeds() {
 		if (deception_counter >= 10)
 			return;
@@ -167,6 +214,10 @@ public class Gameplay {
 		deception_counter++;
 	}
 	
+	/**
+	 * Checks to see if the player has been caught
+	 * @return true if player has been caught
+	 */
 	private boolean checkCaught() {
 		if (caught_probability >= 100) {
 			System.out.println("You have been caught by the SEC! Game Over");
@@ -216,6 +267,11 @@ public class Gameplay {
 		System.out.println("You selected " + player.getName());
 	}
 	
+	/**
+	 * Asks the user to choose a starting location and makes the starting location
+	 * this choice
+	 * @param locations Hashmap of all possible locations
+	 */
 	private void chooseStartLocation(HashMap<String, Location> locations) {
 		System.out.println("Select the name of the location you would like to begin your Ponzi Scheme in");
 		for (Location l : locations.values()) {
